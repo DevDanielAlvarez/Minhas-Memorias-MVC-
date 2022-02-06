a18eddba33cec58816f7c6f33239936c872cd999
DanielDevMuitomoney <91484468+DanielDevMuitomoney@users.noreply.github.com>
2022-02-06 13:50:28 -0300

    cadastro com atributo finalizado

--- App/controllers/users.php
+++ App/controllers/users.php
@@ -4,7 +4,7 @@ use App\Core\Model;
 
 class users extends controller
 {
-    protected $nome;
+   
 
     public function Cadastrar()
     {
@@ -12,38 +12,36 @@ class users extends controller
         //verifica se o button foi acionado, e se algum campo esta vazio.
         
       if(isset($_POST['nome_paciente']) ):
+        $user=$this->Model('user');
                #filtragrem de valores
-               $nome_paciente= addslashes($_POST['nome_paciente']);
-               $telefone_paciente= addslashes($_POST['telefone_paciente']);
-               $dt_paciente= addslashes($_POST['dt_paciente']);
-               $email_paciente= addslashes($_POST['email_paciente']);
-               $patologia_paciente= addslashes($_POST['patologia_paciente']);
-               $estado_paciente= addslashes($_POST['estado_paciente']);
-               $senha1_paciente= addslashes($_POST['senha1_paciente']);
+               $this->nome_paciente= addslashes($_POST['nome_paciente']);
+               $this->telefone_paciente= addslashes($_POST['telefone_paciente']);
+               $this->dt_paciente= addslashes($_POST['dt_paciente']);
+               $this->email_paciente= addslashes($_POST['email_paciente']);
+               $this->patologia_paciente= addslashes($_POST['patologia_paciente']);
+               $this->estado_paciente= addslashes($_POST['estado_paciente']);
+               $this->senha1_paciente= addslashes($_POST['senha1_paciente']);
               
                //familiar
-               $nome_familiar= addslashes($_POST['nome_familiar']);
-               $telefone_familiar= addslashes($_POST['telefone_familiar']);
-               $dt_familiar= addslashes($_POST['dt_familiar']);
-               $email_familiar= addslashes($_POST['email_familiar']);
-               $parent_familiar= addslashes($_POST['parent_familiar']);
-               $estado_familiar= addslashes($_POST['estado_familiar']);
-               $senha1_familiar= addslashes($_POST['senha1_familiar']);
+               $this->nome_familiar= addslashes($_POST['nome_familiar']);
+               $this->telefone_familiar= addslashes($_POST['telefone_familiar']);
+               $this->dt_familiar= addslashes($_POST['dt_familiar']);
+               $this->email_familiar= addslashes($_POST['email_familiar']);
+               $this->parent_familiar= addslashes($_POST['parent_familiar']);
+               $this->estado_familiar= addslashes($_POST['estado_familiar']);
+               $this->senha1_familiar= addslashes($_POST['senha1_familiar']);
               
-               if(empty($nome_paciente)||empty($telefone_paciente)||empty($dt_paciente)||empty($email_paciente)||empty($patologia_paciente)||empty($estado_paciente) ||empty($senha1_paciente) /*family-> */|| empty($nome_familiar)|| empty($telefone_familiar) || empty($dt_familiar) || empty($email_familiar) || empty($parent_familiar) || empty($estado_familiar)|| empty($senha1_familiar)):
+               if(empty($this->nome_paciente)||empty($this->telefone_paciente)||empty($this->dt_paciente)||empty($this->email_paciente)||empty($this->patologia_paciente)||empty($this->estado_paciente) ||empty($this->senha1_paciente) /*family-> */|| empty($this->nome_familiar)|| empty($this->telefone_familiar) || empty($this->dt_familiar) || empty($this->email_familiar) || empty($this->parent_familiar) || empty($this->estado_familiar)|| empty($this->senha1_familiar)):
                 echo "bvbv";
                else:
-                $user=$this->Model('user');
-                $user->nome_paciente=$nome_paciente;
-                $user->telefone_paciente=$telefone_paciente;
-                $user->dt_paciente=$dt_paciente;
-                $user->email_paciente=$email_paciente;
-                $user->patologia_paciente=$patologia_paciente;
-                $user->estado_paciente=$estado_paciente;
-                echo 
-               
                 
-               $user->cadastroUser();        
+                $user->nome_paciente=$this->nome_paciente;
+                $user->telefone_paciente=$this->telefone_paciente;
+                $user->dt_paciente=$this->dt_paciente;
+                $user->email_paciente=$this->email_paciente;
+                $user->patologia_paciente=$this->patologia_paciente;
+                $user->estado_paciente=$this->estado_paciente;
+                $user->cadastroUser();        
         endif;
          else:$this->view('users/cadastro');
 
@@ -54,6 +52,8 @@ class users extends controller
     {
 
         $this->view('users/login');
+
+       
     }
 
 }
