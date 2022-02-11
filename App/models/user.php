3d5e65affd925f163236411a4e4f1350c956b560
DanielDevMuitomoney <91484468+DanielDevMuitomoney@users.noreply.github.com>
2022-02-10 22:32:08 -0300

    cadastro 60%

--- App/models/user.php
+++ App/models/user.php
@@ -8,7 +8,7 @@ Class user extends Controller
     public function cadastroUser()
     {
         try
-        {
+        {    #cadastro do paciente
             $sql="INSERT into tb_paciente (nm_nome,tel_telefone,dt_nascimento,email,patologia,estado,senha) VALUES (?,?,?,?,?,?,?);";
             $stmt= Model::getConn()->prepare($sql);
             $stmt->bindValue(1,$this->nome_paciente);
@@ -19,6 +19,21 @@ Class user extends Controller
             $stmt->bindValue(6,$this->estado_paciente);
             $stmt->bindValue(7,$this->senha1_paciente);
             $stmt->execute();
+            #se o comando der true, pegar o id do user que acaba de ser cadastrado, afim de linkar este id ao familiar
+            if($stmt->execute()):
+                $sql='select id from tb_paciente where email=?';
+                $stmt= Model::getConn()->prepare($sql);
+                $stmt->bindValue(1,$this->email_paciente);
+                $stmt->execute();
+                $resultado=$stmt->fetch(\PDO::FETCH_ASSOC);
+                $id=$resultado['id'];
+                echo $id; 
+                /*-----------------------------
+                #Com o id do user fazer o insert do familiar, junto ao campo ('id_paciente'); 
+                -------------------------------*/
+                
+            endif;
+
             return "Sucesso ao cadastrar";
             #senha rash (cadastrar sem o uso do md5 do mysql);
         }
--- memoria_banco.sql
+++ memoria_banco.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 09-Fev-2022 às 10:12
+-- Tempo de geração: 11-Fev-2022 às 02:30
 -- Versão do servidor: 10.4.22-MariaDB
 -- versão do PHP: 8.1.1
 
@@ -20,6 +20,20 @@ SET time_zone = "+00:00";
 --
 -- Banco de dados: `memoria_banco`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `tb_administrador`
+--
+
+CREATE TABLE `tb_administrador` (
+  `id` int(11) NOT NULL,
+  `nm_nome` varchar(90) NOT NULL,
+  `email` varchar(110) NOT NULL,
+  `estado` char(2) NOT NULL,
+  `senha` varchar(65) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
@@ -42,6 +56,19 @@ CREATE TABLE `tb_familiares` (
 -- --------------------------------------------------------
 
 --
+-- Estrutura da tabela `tb_jogos`
+--
+
+CREATE TABLE `tb_jogos` (
+  `id` int(11) NOT NULL,
+  `nm_jogo` varchar(90) DEFAULT NULL,
+  `num_pontuacao` int(4) DEFAULT NULL,
+  `id_paciente` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
 -- Estrutura da tabela `tb_medicos`
 --
 
@@ -54,7 +81,8 @@ CREATE TABLE `tb_medicos` (
   `dt_nascimento` datetime NOT NULL,
   `ano_trabalho` datetime NOT NULL,
   `especialidade` varchar(65) NOT NULL,
-  `id_instituicao` int(11) DEFAULT NULL
+  `id_instituicao` int(11) DEFAULT NULL,
+  `id_paciente` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
@@ -71,45 +99,76 @@ CREATE TABLE `tb_paciente` (
   `email` varchar(90) DEFAULT NULL,
   `patologia` varchar(90) DEFAULT NULL,
   `estado` varchar(2) DEFAULT NULL,
-  `senha` varchar(60) DEFAULT NULL
+  `senha` varchar(60) DEFAULT NULL,
+  `id_familiar` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Extraindo dados da tabela `tb_paciente`
 --
 
-INSERT INTO `tb_paciente` (`id`, `nm_nome`, `tel_telefone`, `dt_nascimento`, `email`, `patologia`, `estado`, `senha`) VALUES
-(1, '2', '2', '0000-00-00 00:00:00', '2', '2', '2', '123'),
-(2, '2', '2', '0000-00-00 00:00:00', '2', '2', '2', '123'),
-(3, '2', '2', '0000-00-00 00:00:00', '2', '2', '2', '123'),
-(4, '2', '2', '0000-00-00 00:00:00', '2', '2', '2', '123'),
-(5, '2', '2', '0000-00-00 00:00:00', '2', '2', '2', '123'),
-(6, '2', '2', '0000-00-00 00:00:00', '2', '2', '2', '123'),
-(7, '2', '2', '0000-00-00 00:00:00', '2', '2', '2', '123'),
-(8, '2', '2', '0000-00-00 00:00:00', '2', '2', '2', '123'),
-(9, '2', '2', '0000-00-00 00:00:00', '2', '2', '2', '123'),
-(10, '9', '9', '0000-00-00 00:00:00', '9', '9', '9', '123'),
-(11, '2', '2', '0000-00-00 00:00:00', '2', '2', '2', '$2y$10$xyyzjwix7uUHb0C890qTz.BjaAtccte9H2I4oz7CsFn8eC1./NLZG'),
-(12, '2', '2', '0000-00-00 00:00:00', '2', '2', '2', '$2y$10$Gyegwadq/e6nXrRKqZl7uu47lu4HA9YDfNCEqVIKAW0uK4E5wtJUW'),
-(13, '7', '7', '0000-00-00 00:00:00', '7', '7', '7', '$2y$10$3WBHuzsRR1G9deO8jkNebuagjr3Iga3O.EY5IA.JEL.aZeT.ISeNi'),
-(14, '789', '789', '0000-00-00 00:00:00', '789', '789', '78', '$2y$10$4/gCQn3udlagOsmZkt.OK.hNRNaXFi79tfpEbxZFpcIu13h98i1P.'),
-(15, '159', '159', '0000-00-00 00:00:00', '159', '159', '15', '$2y$10$XQIi1/bQzT8.xyBqW9w9FO8pRfH.MUwS7hUPIXTmhLBX.zd8Mzvpy');
+INSERT INTO `tb_paciente` (`id`, `nm_nome`, `tel_telefone`, `dt_nascimento`, `email`, `patologia`, `estado`, `senha`, `id_familiar`) VALUES
+(1, '2', '2', '0000-00-00 00:00:00', '2', '2', '2', '123', NULL),
+(2, '2', '2', '0000-00-00 00:00:00', '2', '2', '2', '123', NULL),
+(3, '2', '2', '0000-00-00 00:00:00', '2', '2', '2', '123', NULL),
+(4, '2', '2', '0000-00-00 00:00:00', '2', '2', '2', '123', NULL),
+(5, '2', '2', '0000-00-00 00:00:00', '2', '2', '2', '123', NULL),
+(6, '2', '2', '0000-00-00 00:00:00', '2', '2', '2', '123', NULL),
+(7, '2', '2', '0000-00-00 00:00:00', '2', '2', '2', '123', NULL),
+(8, '2', '2', '0000-00-00 00:00:00', '2', '2', '2', '123', NULL),
+(9, '2', '2', '0000-00-00 00:00:00', '2', '2', '2', '123', NULL),
+(10, '9', '9', '0000-00-00 00:00:00', '9', '9', '9', '123', NULL),
+(11, '2', '2', '0000-00-00 00:00:00', '2', '2', '2', '$2y$10$xyyzjwix7uUHb0C890qTz.BjaAtccte9H2I4oz7CsFn8eC1./NLZG', NULL),
+(12, '2', '2', '0000-00-00 00:00:00', '2', '2', '2', '$2y$10$Gyegwadq/e6nXrRKqZl7uu47lu4HA9YDfNCEqVIKAW0uK4E5wtJUW', NULL),
+(13, '7', '7', '0000-00-00 00:00:00', '7', '7', '7', '$2y$10$3WBHuzsRR1G9deO8jkNebuagjr3Iga3O.EY5IA.JEL.aZeT.ISeNi', NULL),
+(14, '789', '789', '0000-00-00 00:00:00', '789', '789', '78', '$2y$10$4/gCQn3udlagOsmZkt.OK.hNRNaXFi79tfpEbxZFpcIu13h98i1P.', NULL),
+(15, '159', '159', '0000-00-00 00:00:00', '159', '159', '15', '$2y$10$XQIi1/bQzT8.xyBqW9w9FO8pRfH.MUwS7hUPIXTmhLBX.zd8Mzvpy', NULL),
+(16, '4546', '4546', '0000-00-00 00:00:00', '4546', '4546', '45', '$2y$10$s1CDMqsuB6N.k8mP9jRf8uXhWvy8hGoopqQWIhXFtTjnTWuknEsQG', NULL),
+(17, '4546', '4546', '0000-00-00 00:00:00', '4546', '4546', '45', '$2y$10$s1CDMqsuB6N.k8mP9jRf8uXhWvy8hGoopqQWIhXFtTjnTWuknEsQG', NULL),
+(18, '4546', '4546', '0000-00-00 00:00:00', '4546', '4546', '45', '$2y$10$E3eWH3Peg/ntDfnXmP4AJOcfiTu2U3XrYP0hFMV8K6HoYZbtUS8fu', NULL),
+(19, '4546', '4546', '0000-00-00 00:00:00', '4546', '4546', '45', '$2y$10$E3eWH3Peg/ntDfnXmP4AJOcfiTu2U3XrYP0hFMV8K6HoYZbtUS8fu', NULL),
+(20, '4546', '4546', '0000-00-00 00:00:00', '4546', '4546', '45', '$2y$10$bD9aynCQm54Hw9cpuEuOMuMutQ2DYGrsbwtXBaxCot4KJzilrQ1Le', NULL),
+(21, '4546', '4546', '0000-00-00 00:00:00', '4546', '4546', '45', '$2y$10$bD9aynCQm54Hw9cpuEuOMuMutQ2DYGrsbwtXBaxCot4KJzilrQ1Le', NULL),
+(22, '4546', '4546', '0000-00-00 00:00:00', '4546', '4546', '45', '$2y$10$6taSkCal28fYjTU54abQhushJlMGSs8NxbsoxuDdXjBSnRrPCxBfG', NULL),
+(23, '4546', '4546', '0000-00-00 00:00:00', '4546', '4546', '45', '$2y$10$6taSkCal28fYjTU54abQhushJlMGSs8NxbsoxuDdXjBSnRrPCxBfG', NULL),
+(24, '4546', '4546', '0000-00-00 00:00:00', '4546', '4546', '45', '$2y$10$NgGel8eVxdM9kCkNMtrvBujIEfrP7hBO6PHynkuH2Sq33WFhvDFIe', NULL),
+(25, '4546', '4546', '0000-00-00 00:00:00', '4546', '4546', '45', '$2y$10$NgGel8eVxdM9kCkNMtrvBujIEfrP7hBO6PHynkuH2Sq33WFhvDFIe', NULL),
+(26, '4546', '4546', '0000-00-00 00:00:00', '4546', '4546', '45', '$2y$10$XZclSoKJf8kwsJIS.fXGC.KUBvC9VfOo14jes3p7biiHgp189QVtS', NULL),
+(27, '4546', '4546', '0000-00-00 00:00:00', '4546', '4546', '45', '$2y$10$XZclSoKJf8kwsJIS.fXGC.KUBvC9VfOo14jes3p7biiHgp189QVtS', NULL),
+(28, '4546', '4546', '0000-00-00 00:00:00', '4546', '4546', '45', '$2y$10$Q1zHVC5CpxZoEf2i2L1hROvqhF9eROVMdKj1D/VrTRcjW97CHBohq', NULL),
+(29, '4546', '4546', '0000-00-00 00:00:00', '4546', '4546', '45', '$2y$10$Q1zHVC5CpxZoEf2i2L1hROvqhF9eROVMdKj1D/VrTRcjW97CHBohq', NULL),
+(30, '4546', '4546', '0000-00-00 00:00:00', '4546', '4546', '45', '$2y$10$rx0leyY1blmujXm/NKnXeuWUWmbreycRlKD6Slr4o84T8PTZgQ4Oy', NULL),
+(31, '4546', '4546', '0000-00-00 00:00:00', '4546', '4546', '45', '$2y$10$rx0leyY1blmujXm/NKnXeuWUWmbreycRlKD6Slr4o84T8PTZgQ4Oy', NULL);
 
 --
 -- Índices para tabelas despejadas
 --
 
 --
+-- Índices para tabela `tb_administrador`
+--
+ALTER TABLE `tb_administrador`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Índices para tabela `tb_familiares`
 --
 ALTER TABLE `tb_familiares`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_paciente` (`id_paciente`);
+
+--
+-- Índices para tabela `tb_jogos`
+--
+ALTER TABLE `tb_jogos`
   ADD PRIMARY KEY (`id`);
 
 --
 -- Índices para tabela `tb_medicos`
 --
 ALTER TABLE `tb_medicos`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_paciente_medico` (`id_paciente`);
 
 --
 -- Índices para tabela `tb_paciente`
@@ -122,9 +181,21 @@ ALTER TABLE `tb_paciente`
 --
 
 --
+-- AUTO_INCREMENT de tabela `tb_administrador`
+--
+ALTER TABLE `tb_administrador`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT de tabela `tb_familiares`
 --
 ALTER TABLE `tb_familiares`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `tb_jogos`
+--
+ALTER TABLE `tb_jogos`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
@@ -137,7 +208,23 @@ ALTER TABLE `tb_medicos`
 -- AUTO_INCREMENT de tabela `tb_paciente`
 --
 ALTER TABLE `tb_paciente`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=32;
+
+--
+-- Restrições para despejos de tabelas
+--
+
+--
+-- Limitadores para a tabela `tb_familiares`
+--
+ALTER TABLE `tb_familiares`
+  ADD CONSTRAINT `id_paciente` FOREIGN KEY (`id_paciente`) REFERENCES `tb_paciente` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+--
+-- Limitadores para a tabela `tb_medicos`
+--
+ALTER TABLE `tb_medicos`
+  ADD CONSTRAINT `id_paciente_medico` FOREIGN KEY (`id_paciente`) REFERENCES `tb_paciente` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
