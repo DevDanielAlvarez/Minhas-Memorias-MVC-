88ccbf7fc1533dab418f16fa5f413a6a48f68d1d
DanielDevMuitomoney <91484468+DanielDevMuitomoney@users.noreply.github.com>
2022-05-08 14:37:21 -0300

    index of games

--- App/views/games/index.php
+++ App/views/games/index.php
@@ -2,10 +2,10 @@
 
 <div class="container">
 <div class="card text-center">
-  <div class="card-header">
+  <div class="card-header" style="background-color:#2E3DF0;color:white;">
     Jogo de memorização
   </div>
-  <div class="card-body">
+  <div class="card-body" style="background-color: purple;color:white;">
     <h5 class="card-title">Jogo da Memória</h5>
     <p class="card-text">Neste jogo será dada um número especifico de cartas que terão que ser combinadas</p>
     <a href="/games/jogo_da_memoria" class="btn btn-primary">JOGAR</a>
